6958ce1c7cf479d7fb66a434bfcf619df8b51255
AnilEmreAy <152299007+AnilEmreAy@users.noreply.github.com>
2025-05-01 23:46:11 +0300

    Add files via upload

---
+++ DGD208-Spring2025-AnilEmreAy/DGD208-Spring2025/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DGD208_Spring2025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
